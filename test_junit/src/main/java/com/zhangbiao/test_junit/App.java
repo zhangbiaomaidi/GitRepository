a0b5bccc470b9e216d511e086f84040aea8f971b
zhangbiao <2374233187@qq.com>
2017-07-14 17:50:34 +0800

    将test_junit测试项目导入git中

---
+++ test_junit/src/main/java/com/zhangbiao/test_junit/App.java
@@ -0,0 +1,13 @@
+package com.zhangbiao.test_junit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public void print()
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ test_junit/src/main/java/com/zhangbiao/test_junit/AppTest.java
@@ -0,0 +1,20 @@
+package com.zhangbiao.test_junit;
+
+ 
+
+import org.junit.Test;
+
+public class AppTest {
+
+	App app = new App();
+	
+	@Test
+	public void testPrint() {
+	   	
+		app.print();
+	}
+	
+	
+	
+
+}
